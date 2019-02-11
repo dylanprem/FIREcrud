f1207ca68e060827f131c86831a701d7ce85f896
dylan prem <dylanprem90@gmail.com>
2019-02-10 21:39:40 -0500

    initial setup

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyCedQtQTjM4fLMIQltAr66vQynojsjl-os",
+  authDomain: "firecrud-72def.firebaseapp.com",
+  databaseURL: "https://firecrud-72def.firebaseio.com",
+  projectId: "firecrud-72def",
+  storageBucket: "firecrud-72def.appspot.com",
+  messagingSenderId: "493459915295"
+};
