8c3aa70a16e51b7d31ab8eaf17090796d886ac8c
dylan <dylanprem90@gmail.com>
2019-02-14 10:55:14 -0500

    re-structured directories

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyCedQtQTjM4fLMIQltAr66vQynojsjl-os",
-  authDomain: "firecrud-72def.firebaseapp.com",
-  databaseURL: "https://firecrud-72def.firebaseio.com",
-  projectId: "firecrud-72def",
-  storageBucket: "firecrud-72def.appspot.com",
-  messagingSenderId: "493459915295"
-};
